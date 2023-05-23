8e7303bfc61130b265f5c5ecf4fbc2821964e99c
Szabolcs Gál <sgal@cloudera.com>
2023-05-23 12:20:11 +0200

    HDDS-8588 Add trust chain building for legacy case

--- hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/client/DefaultCertificateClient.java
+++ hadoop-hdds/framework/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/client/DefaultCertificateClient.java
@@ -48,6 +48,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
@@ -338,10 +339,46 @@ public abstract class DefaultCertificateClient implements CertificateClient {
     }
 
     List<X509Certificate> chain = new ArrayList<>();
-    for (int i = 0; i < path.getCertificates().size(); i++) {
-      chain.add((X509Certificate) path.getCertificates().get(i));
+    // certificate bundle case
+    if (path.getCertificates().size() > 1) {
+      for (int i = 0; i < path.getCertificates().size(); i++) {
+        chain.add((X509Certificate) path.getCertificates().get(i));
+      }
+    } else {
+      // case before certificate bundle is supported
+      X509Certificate lastInsertedCert = getCertificate();
+      while (!isRootCa(lastInsertedCert)) {
+        Optional<X509Certificate> issuerOpt =
+            getIssuerForCert(lastInsertedCert, getAllCaCerts());
+        if (issuerOpt.isPresent()) {
+          X509Certificate issuer = issuerOpt.get();
+          chain.add(issuer);
+          lastInsertedCert = issuer;
+        } else {
+          throw new RuntimeException("No issuer found for certificate: " +
+              lastInsertedCert);
+        }
+      }
+      //add self signed certificate to the chain
+      chain.add(lastInsertedCert);
     }
+
     return chain;
+  }
+
+  private boolean isRootCa(X509Certificate cert) {
+    return cert.getSubjectX500Principal().equals(cert.getIssuerX500Principal());
+  }
+
+  private Optional<X509Certificate> getIssuerForCert(X509Certificate cert,
+      Set<X509Certificate> issuerCerts) {
+    for (X509Certificate issuer : issuerCerts) {
+      if (cert.getIssuerX500Principal().equals(
+          issuer.getSubjectX500Principal())) {
+        return Optional.of(issuer);
+      }
+    }
+    return Optional.empty();
   }
 
   @Override
@@ -539,6 +576,9 @@ public abstract class DefaultCertificateClient implements CertificateClient {
       if (addToCertMap) {
         certificateMap.putIfAbsent(
             cert.getSerialNumber().toString(), certificatePath);
+        if (caType == CAType.SUBORDINATE || caType == CAType.ROOT) {
+          caCertificates.add(certificatePath);
+        }
       }
     } catch (IOException | java.security.cert.CertificateException e) {
       throw new CertificateException("Error while storing certificate.", e,
