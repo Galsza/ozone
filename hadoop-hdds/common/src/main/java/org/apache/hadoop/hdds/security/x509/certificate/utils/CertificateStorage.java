3ce7e55b7c2eae463642f42d85eeab89d5cd1430
Szabolcs Gál <sgal@cloudera.com>
2024-05-21 17:52:05 +0200

    create certstorage

--- hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/utils/CertificateStorage.java
+++ hadoop-hdds/common/src/main/java/org/apache/hadoop/hdds/security/x509/certificate/utils/CertificateStorage.java
@@ -18,31 +18,44 @@
 package org.apache.hadoop.hdds.security.x509.certificate.utils;
 
 import org.apache.hadoop.hdds.security.SecurityConfig;
+import org.apache.hadoop.hdds.security.x509.certificate.authority.CAType;
+import org.apache.hadoop.hdds.security.x509.exception.CertificateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
+import java.security.cert.CertPath;
+import java.security.cert.X509Certificate;
+import java.util.Map;
 import java.util.Set;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.nio.file.attribute.PosixFilePermission.OWNER_READ;
 import static java.nio.file.attribute.PosixFilePermission.OWNER_WRITE;
 import static java.nio.file.attribute.PosixFilePermission.OWNER_EXECUTE;
+import static org.apache.hadoop.hdds.security.x509.exception.CertificateException.ErrorCode.CERTIFICATE_ERROR;
 
 /**
  * Class for storing certificates to disk.
  */
 public class CertificateStorage {
 
+  private static final String CERT_FILE_EXTENSION = ".crt";
+  public static final String CERT_FILE_NAME_FORMAT = "%s" + CERT_FILE_EXTENSION;
+  
   public static final Charset DEFAULT_CHARSET = StandardCharsets.UTF_8;
+  private final Logger logger;
 
   private static final Logger LOG =
       LoggerFactory.getLogger(CertificateStorage.class);
@@ -51,12 +64,20 @@ public class CertificateStorage {
       Stream.of(OWNER_READ, OWNER_WRITE, OWNER_EXECUTE)
           .collect(Collectors.toSet());
 
-  private final SecurityConfig config;
-  private final Path location;
+  private CertPath certPath;
+  private Map<String, CertPath> certificateMap;
+  private Set<X509Certificate> rootCaCertificates;
+  private Set<X509Certificate> caCertificates;
+  private SecurityConfig config;
+  private Path location;
+  private String certSerialId;
+  private String caCertId;
+  private String rootCaCertId;
 
-  public CertificateStorage(SecurityConfig conf, Path location) {
+  public CertificateStorage(SecurityConfig conf, Path location, Logger log) {
     this.config = conf;
     this.location = location;
+    this.logger = log;
   }
 
   /**
@@ -81,6 +102,108 @@ public class CertificateStorage {
     Files.setPosixFilePermissions(certificateFile.toPath(), PERMISSION_SET);
   }
 
+  public synchronized void storeCertificate(String pemEncodedCert,
+                                            CAType caType, CertificateCodec codec, boolean addToCertMap,
+                                            boolean updateCA) throws CertificateException {
+    try {
+      CertPath certificatePath =
+          CertificateCodec.getCertPathFromPemEncodedString(pemEncodedCert);
+      X509Certificate cert = firstCertificateFrom(certificatePath);
+
+      String certName = String.format(CERT_FILE_NAME_FORMAT,
+          caType.getFileNamePrefix() + cert.getSerialNumber().toString());
+
+      if (updateCA) {
+        if (caType == CAType.SUBORDINATE) {
+          caCertId = cert.getSerialNumber().toString();
+        }
+        if (caType == CAType.ROOT) {
+          rootCaCertId = cert.getSerialNumber().toString();
+        }
+      }
+
+      CertificateCodec.writeCertificate(
+          Paths.get(codec.getLocation().toAbsolutePath().toString(), certName), pemEncodedCert);
+      if (addToCertMap) {
+        certificateMap.put(cert.getSerialNumber().toString(), certificatePath);
+        if (caType == CAType.SUBORDINATE) {
+          caCertificates.add(cert);
+        }
+        if (caType == CAType.ROOT) {
+          rootCaCertificates.add(cert);
+        }
+      }
+    } catch (IOException | java.security.cert.CertificateException e) {
+      throw new CertificateException("Error while storing certificate.", e,
+          CERTIFICATE_ERROR);
+    }
+  }
+
+
+  private synchronized void readCertificateFile(Path filePath) {
+    CertificateCodec codec = new CertificateCodec(config, location);
+    String fileName = filePath.getFileName().toString();
+
+    X509Certificate cert;
+    try {
+      CertPath allCertificates = codec.getCertPath(fileName);
+      cert = firstCertificateFrom(allCertificates);
+      String readCertSerialId = cert.getSerialNumber().toString();
+
+      if (readCertSerialId.equals(certSerialId)) {
+        this.certPath = allCertificates;
+      }
+      certificateMap.put(readCertSerialId, allCertificates);
+      addCertsToSubCaMapIfNeeded(fileName, allCertificates);
+      addCertToRootCaMapIfNeeded(fileName, allCertificates);
+
+      updateCachedData(fileName, CAType.SUBORDINATE, this::updateCachedSubCAId);
+      updateCachedData(fileName, CAType.ROOT, this::updateCachedRootCAId);
+
+      logger.info("Added certificate {} from file: {}.", readCertSerialId,
+          filePath.toAbsolutePath());
+      if (logger.isDebugEnabled()) {
+        logger.debug("Certificate: {}", cert);
+      }
+    } catch (java.security.cert.CertificateException
+             | IOException | IndexOutOfBoundsException e) {
+      logger.error("Error reading certificate from file: {}.",
+          filePath.toAbsolutePath(), e);
+    }
+  }
+
+  private synchronized void updateCachedRootCAId(String s) {
+    BigInteger candidateNewId = new BigInteger(s);
+    if (rootCaCertId == null
+        || new BigInteger(rootCaCertId).compareTo(candidateNewId) < 0) {
+      rootCaCertId = s;
+    }
+  }
+
+  private synchronized void updateCachedSubCAId(String s) {
+    BigInteger candidateNewId = new BigInteger(s);
+    if (caCertId == null
+        || new BigInteger(caCertId).compareTo(candidateNewId) < 0) {
+      caCertId = s;
+    }
+  }
+
+  private void updateCachedData(
+      String fileName,
+      CAType tryCAType,
+      Consumer<String> updateCachedId
+  ) throws IOException {
+    String caTypePrefix = tryCAType.getFileNamePrefix();
+
+    if (fileName.startsWith(caTypePrefix)) {
+      updateCachedId.accept(
+          fileName.substring(caTypePrefix.length(),
+              fileName.length() - CERT_FILE_EXTENSION.length()
+          ));
+    }
+  }
+
+
   private static void checkBasePathDirectory(Path basePath) throws IOException {
     if (!basePath.toFile().exists()) {
       if (!basePath.toFile().mkdirs()) {
@@ -89,5 +212,24 @@ public class CertificateStorage {
             + basePath);
       }
     }
+  }
+
+  private void addCertsToSubCaMapIfNeeded(String fileName, CertPath certs) {
+    if (fileName.startsWith(CAType.SUBORDINATE.getFileNamePrefix())) {
+      caCertificates.addAll(
+          certs.getCertificates().stream()
+              .map(x -> (X509Certificate) x)
+              .collect(Collectors.toSet()));
+    }
+  }
+
+  private void addCertToRootCaMapIfNeeded(String fileName, CertPath certs) {
+    if (fileName.startsWith(CAType.ROOT.getFileNamePrefix())) {
+      rootCaCertificates.add(firstCertificateFrom(certs));
+    }
+  }
+
+  private static X509Certificate firstCertificateFrom(CertPath certificatePath) {
+    return (X509Certificate) certificatePath.getCertificates().get(0);
   }
 }
